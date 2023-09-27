bc2419fde0585225ff42bab0b29a82f83c979121
AlnurM <alnur.mustafayev@gmail.com>
2023-09-27 09:17:18 +0600

    fix: i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 
 const nextConfig = {
   i18n: {
     locales: ['ru', 'kz', 'en'],
     defaultLocale: 'ru',
     localeDetection: false,
+    localePath: path.resolve('./public/locales'),
   },
 }
 
